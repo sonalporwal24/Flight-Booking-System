e7ad579d4ff606c276848a02b5af876882449ee5
Papan Das <167051110+daspapan@users.noreply.github.com>
2025-01-21 19:39:50 +0530

    new branch

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CDKContext } from '../bin/backend';
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: cdk.StackProps, context: CDKContext){
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const appName = `${context.appName}-${context.stage}`;
+        console.log(`AppName -> ${appName}`)
+        
+    }
 }
